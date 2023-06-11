2d90268f40b8d425a9b5e342e519ed8edfabe594
Zach Champion <zachchampion79@gmail.com>
2023-06-10 22:33:09 -0400

    Structure main

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,9 @@
 #include <LimitSwitch.hpp>
 #include <ar3_error.h>
 
-#define JOINT_COUNT (sizeof(joints) / sizeof(JointConfig))
+#define SIZE(x) (sizeof(x) / sizeof(x[0]))
+#define JOINT_COUNT SIZE(joints)
 
-/**
- * Configuration for a joint. This includes stepper, encoder, and limit switch information.
- */
 struct JointConfig {
   int min_steps;               // Most negative position of the joint (in steps)
   int max_steps;               // Most positive position of the joint (in steps)
@@ -29,6 +27,20 @@ struct JointConfig {
   uint16_t lim_debounce_time;  // Debounce time for the limit switch
 
   uint8_t reversed;            // Whether the joint is reversed
+};
+
+enum joint_state_t {
+  JOINT_STATE_OFF,
+  JOINT_STATE_HOLD,
+  JOINT_STATE_MOVE_SPEED,
+  JOINT_STATE_MOVE_AUTO,
+};
+
+struct JointState {
+  int steps;            // Current position of the joint (in steps)
+  float speed;          // Current speed of the joint (in steps per second)
+
+  joint_state_t state;  // Current state of the joint
 };
 
 // ============================ ROBOT CONFIGURATION ============================
@@ -57,14 +69,14 @@ static const JointConfig joints[] = {
 static AccelStepper* steppers[JOINT_COUNT];
 static Encoder* encoders[JOINT_COUNT];
 static LimitSwitch* limit_switches[JOINT_COUNT];
+static JointState* joint_states[JOINT_COUNT];
 
 // Serial buffer used for receiving commands
-static char ibuf[256];
+static char ibuf[1024];
 static uint8_t ibuf_idx = 0;
 
 // Serial buffer used for sending responses
-static char obuf[256];
-static uint8_t obuf_idx = 0;
+static char obuf[1024];
 
 void setup()
 {
@@ -75,7 +87,107 @@ void setup()
     steppers[i]->setAcceleration(joints[i].max_accel);
     encoders[i] = new Encoder(joints[i].enc_a_pin, joints[i].enc_b_pin);
     limit_switches[i] = new LimitSwitch(joints[i].lim_pin);
+    joint_states[i] = new JointState{
+      .steps = 0,
+      .speed = 0,
+      .state = JOINT_STATE_OFF,
+    };
   }
 }
 
-void loop() {}
+void loop()
+{
+  // Read a character from the serial port
+  if (Serial.available()) {
+    char c = Serial.read();
+
+    // If the character is a newline, then we have received a complete command
+    if (c == '\n') {
+      ibuf[ibuf_idx] = '\0';
+      parse_serial(obuf, SIZE(obuf), ibuf, ibuf_idx);
+      send_msg(obuf);
+      ibuf_idx = 0;
+      goto update_hw;
+    }
+
+    // If the character is whitespace, then ignore it
+    if (c == ' ' || c == '\t' || c == '\r') {
+      goto update_hw;
+    }
+
+    // If the buffer is full, send an error and reset the buffer
+    if (ibuf_idx >= SIZE(ibuf)) {
+      write_err(obuf, SIZE(obuf), AR3_ERR_BUFFER_OVERFLOW, "Command buffer overflow");
+      send_msg(obuf);
+      ibuf_idx = 0;
+      goto update_hw;
+    }
+
+    // If we are currently receiving a command, or this is the start of a new command,
+    // then add the character to the buffer
+    if (ibuf_idx > 0 || c == '$') {
+      ibuf[ibuf_idx++] = c;
+      goto update_hw;
+    }
+  }
+
+update_hw:
+  run_steppers();
+}
+
+/**
+ * Parse an input from the serial port.
+ *
+ * \param[out] dest Destination to write the response to
+ * \param[in] size Size of the destination buffer
+ * \param[in] from Input buffer to parse
+ * \param[in] len Length of the input buffer
+ */
+void parse_serial(char* dest, size_t size, char* from, size_t len)
+{
+  // TODO
+}
+
+/**
+ * Send a message to the serial port. This function adds the '$', the checksum, and the newline.
+ *
+ * \param[in] from Buffer to send.
+ */
+void send_msg(char* from)
+{
+  Serial.print("$");
+  Serial.print(from);
+  Serial.print("\n");
+}
+
+/**
+ * Write an error message to a buffer.
+ *
+ * \param[out] dest The buffer to write to
+ * \param[in] size The size of the buffer
+ * \param[in] code The error code
+ * \param[in] msg The error message
+ */
+int write_err(char* dest, size_t size, uint8_t code, const char* msg)
+{
+  return snprintf(obuf, size, "ERR;%d;%s;", code, msg);
+}
+
+/**
+ * Run the stepper motors. This function should be called frequently.
+ */
+void run_steppers()
+{
+  for (uint8_t i = 0; i < JOINT_COUNT; ++i) {
+    switch (joint_states[i]->state) {
+      case JOINT_STATE_MOVE_AUTO:
+        steppers[i]->run();
+        break;
+      case JOINT_STATE_MOVE_SPEED:
+        steppers[i]->runSpeed();
+        break;
+      default:
+        break;
+    }
+  }
+}
